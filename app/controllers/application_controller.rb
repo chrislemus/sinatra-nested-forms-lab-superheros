37776af3bc54a79268094499a0258d971734e52c
chrislemus <icrislemus@gmail.com>
2021-01-21 09:44:06 -0500

    commit tree 25185e28cec9859e7e69ae0b549be02f15bdb41a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,19 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    
+    get('/') {
+        erb :super_hero
+    }
 
+    post('/teams') {
+        @t = params['team']
+        h = params['team']['members']
+        @h1 = h[0]
+        @h2 = h[1]
+        @h3 = h[2]
+        # puts @h1
+        erb :team
+    }
 
 end
